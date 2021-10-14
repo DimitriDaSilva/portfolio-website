637baea9a3f9646699e388c209f93bdbdca822e1
Dimitri Da Silva <dimitri.gomes.da.silva@gmail.com>
2021-10-14 15:58:38 +0100

    Did navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,41 @@
-//import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components'
 
-import "./App.css";
-import Navbar from "./components/navbar/Navbar";
-import Home from "./pages/home/Home";
-import Projects from "./pages/projects/Projects";
-import About from "./pages/about/About";
-import Error from "./pages/error/Error";
-import Footer from "./components/footer/Footer";
+import GlobalStyle from './global'
+import { themes } from './themes'
+import Navbar from './components/navbar/Navbar';
+import Home from './pages/home/Home';
+import Projects from './pages/projects/Projects';
+import About from './pages/about/About';
+import Error from './pages/error/Error';
+import Footer from './components/footer/Footer';
 
-function App() {
+const App:React.FC = () => {
   return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/projects">
-          <Projects />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route>
-          <Error />
-        </Route>
-      </Switch>
-      <Footer />
-    </Router>
+    <ThemeProvider theme={themes.light}>
+      <>
+        <GlobalStyle />
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/projects">
+              <Projects />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route>
+              <Error />
+            </Route>
+          </Switch>
+          <Footer />
+        </Router>
+      </>
+    </ThemeProvider>
   );
 }
 
