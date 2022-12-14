d6e2839c7d99e3a7aacdd24eaf0b9e091bc859d4
DimitriDaSilva <dimitri.gomes.da.silva@gmail.com>
2022-12-14 09:46:30 +0000

    refactor: make ContactForm more modular

--- src/components/contact-form/index.tsx
+++ src/components/contact-form/index.tsx
@@ -5,6 +5,7 @@ import { useRef } from "react";
 
 import SectionTitle from "../section-title/SectionTitle";
 import Button from "../button/Button";
+import FormField from "./FormField";
 
 const ContactForm: React.FunctionComponent = () => {
   const formRef = useRef<HTMLFormElement | null>(null);
@@ -42,36 +43,22 @@ const ContactForm: React.FunctionComponent = () => {
       </Text>
 
       <Form ref={formRef} onSubmit={sendEmail}>
-        <InputSection>
-          <Input type="text" name="name" id="name" />
+        <FormField formFieldInput="input" type="text" label="Name" id="name" />
 
-          <Label htmlFor="name">
-            <Span>Name</Span>
-          </Label>
-        </InputSection>
+        <FormField
+          formFieldInput="input"
+          type="email"
+          label="Email"
+          id="email"
+        />
 
-        <InputSection>
-          <Input type="email" name="email" id="email" />
-
-          <Label htmlFor="email">
-            <Span>Email</Span>
-          </Label>
-        </InputSection>
-
-        <InputSection>
-          <Input type="text" name="subject" id="subject" />
-
-          <Label htmlFor="subject">
-            <Span>Subject</Span>
-          </Label>
-        </InputSection>
-        <InputSection>
-          <TextArea name="message" id="message" />
-
-          <Label htmlFor="message">
-            <Span>Message</Span>
-          </Label>
-        </InputSection>
+        <FormField
+          formFieldInput="input"
+          type="text"
+          label="Subject"
+          id="subject"
+        />
+        <FormField formFieldInput="textarea" label="Message" id="message" />
 
         <Button text="Send message" type="submit" />
       </Form>
@@ -115,115 +102,4 @@ export const Form = styled.form`
   }
 
   padding-bottom: 40px;
-`;
-
-export const InputSection = styled.div`
-  ${down("tablet")} {
-    width: 100%;
-  }
-
-  ${up("tablet")} {
-    width: 560px;
-  }
-
-  position: relative;
-
-  padding: 10px 0px;
-`;
-
-export const Label = styled.label`
-  align-self: flex-start;
-
-  position: absolute;
-  bottom: 0%;
-  left: 0%;
-  width: 100%;
-  height: 100%;
-
-  font-size: ${({ theme }) => theme.font.sizes.paragraph_m};
-  color: ${({ theme }) => theme.colors.lightGray};
-
-  pointer-events: none;
-
-  border-bottom: 1px solid ${({ theme }) => theme.colors.font};
-
-  overflow: hidden;
-
-  &::after {
-    content: "";
-
-    position: absolute;
-    left: 0px;
-    bottom: -1px;
-
-    height: 100%;
-    width: 100%;
-    border-bottom: 2px solid ${({ theme }) => theme.colors.font};
-
-    transform: translateX(-100%);
-  }
-`;
-
-export const Span = styled.span`
-  position: absolute;
-  bottom: 5px;
-  left: 0px;
-
-  transition: all 0.2s ease;
-`;
-
-export const Input = styled.input.attrs(() => ({
-  autoComplete: "off",
-  required: true,
-}))`
-  width: 100%;
-
-  font-size: ${({ theme }) => theme.font.sizes.paragraph_m};
-  color: ${({ theme }) => theme.colors.font};
-  font-family: ${({ theme }) => theme.font.families.paragraph};
-
-  padding-top: 20px;
-  border: none;
-  outline: none;
-
-  &:focus + ${Label} ${Span}, &:valid + ${Label} ${Span} {
-    transform: translateY(-190%);
-    font-size: ${({ theme }) => theme.font.sizes.paragraph_s};
-  }
-
-  &:focus + ${Label}::after, &:valid + ${Label}::after {
-    transform: translateX(0%);
-  }
-
-  &:not(:focus):invalid {
-    color: ${({ theme }) => theme.colors.bg};
-  }
-`;
-
-export const TextArea = styled.textarea.attrs(() => ({
-  autoComplete: "off",
-  required: true,
-  spellcheck: true,
-}))`
-  width: 100%;
-  height: 100px;
-
-  font-size: ${({ theme }) => theme.font.sizes.paragraph_m};
-  color: ${({ theme }) => theme.colors.font};
-  font-family: ${({ theme }) => theme.font.families.paragraph};
-
-  margin-top: 15px;
-  padding-top: 80px;
-  border: none;
-  outline: none;
-  resize: none;
-
-  &:focus + ${Label} ${Span}, &:valid + ${Label} ${Span} {
-    transform: translateY(-700%);
-    font-size: ${({ theme }) => theme.font.sizes.paragraph_s};
-  }
-
-  &:focus + ${Label}::after, &:valid + ${Label}::after {
-    transform: translateX(0%);
-  }
 `;
